8bc76c85b3358d17992f985df3fd18044ae69d75
Soohyun Mun <50057577+suhyunmoon1998@users.noreply.github.com>
2022-11-18 00:45:29 -0800

    Update project.js

--- maze-runner/project.js
+++ maze-runner/project.js
@@ -25,9 +25,10 @@ export class Project extends Scene {
         this.gray = hex_color("#808080");
 
         this.light_coords = Vector3.cast(
-            [2,1,3],
+             [2,1,3],
             [-2,1,5],
-            [1,1,10]
+            [-3,1,9],
+             [2,1,7]
         );
 
         // *** Materials
@@ -59,6 +60,24 @@ export class Project extends Scene {
         this.BOX_SIZE_units = 2;
 
         this.maze_coords = Vector3.cast(
+            
+            
+            
+            [0,0,1],[this.random_1[1],0,1],[this.random_1[1],0,2],
+           [-5,0,3], [-4,0,3],[-3,0,3], [-2,0,3], [-1,0,3], [0,0,3], [1,0,3],[2,0,3],[3,0,3],    
+            [this.random_2[1],0,4], 
+           [-5,0,5],[-4,0,5],[-3,0,5], [-2,0,5], [-1,0,5], [0,0,5], [1,0,5],[2,0,5],[3,0,5],
+             [this.random_2[2],0,6], 
+           [-5,0,7],[-4,0,7],[-3,0,7], [-2,0,7], [-1,0,7], [0,0,7], [1,0,7],[2,0,7],[3,0,7],
+             [this.random_2[3],0,8], 
+           [-5,0,9],[-4,0,9],[-3,0,9], [-2,0,9], [-1,0,9], [0,0,9], [1,0,9],[2,0,9],[3,0,9],
+             [this.random_2[4],0,10], 
+            [-5,0,11],[-4,0,11],[-3,0,11], [-2,0,11], [-1,0,11], [0,0,11], [1,0,11],[2,0,11],[3,0,11],
+          [this.random_2[5],0,12],
+            [-5,0,13],[-4,0,13],[-3,0,13], [-2,0,13], [-1,0,13], [0,0,13], [1,0,13],[2,0,13],[3,0,13]
+       
+           
+            /*
             [0,0,0],[0,0,1],[1,0,1],[2,0,1],[2,0,2],[2,0,3],[1,0,3],[0,0,3],[-1,0,3],
             [-2,0,3],[-2,0,2],
             [-2,0,1],[-3,0,1],[-4,0,1],[-4,0,2],[-4,0,3],
@@ -67,6 +86,7 @@ export class Project extends Scene {
             [1,0,8],[2,0,8],[2,0,9],[2,0,10],[1,0,10],[0,0,10],[0,0,11],[-1,0,11],
             [-2,0,11],[-2,0,10],[-2,0,9],[-3,0,9],[-4,0,9],
             [-4,0,10],[-4,0,11],[-4,0,12]
+            */
         );
 
         this.ALL_VISIBLE_INTERVAL = 4.0;
@@ -196,7 +216,7 @@ export class Project extends Scene {
 
         // Red end caps
         this.shapes.cube.draw(context, program_state, Mat4.translation(0, 0.5, 2), this.materials.plastic.override({color: red}));
-        this.shapes.cube.draw(context, program_state, Mat4.translation(-8, 0.5, -26), this.materials.plastic.override({color: red}));
+        this.shapes.cube.draw(context, program_state, Mat4.translation(this.random_2[6], 0.5, -26), this.materials.plastic.override({color: red}));
 
         // DRAW SPHERE
         this.shapes.sphere.draw(context, program_state, this.avatar_transform, this.materials.plastic);
